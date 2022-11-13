93073273b45ed9eed1d6191b0a8e3d823706884b
William Berg <William.Berg2@gmail.com>
2022-11-13 13:59:08 +0100

    can now see current playing song

--- lfmds.py
+++ lfmds.py
@@ -2,6 +2,9 @@
 #                                                                               #
 # TODO: Get auth                                                                #
 # TODO: Use command line inputs, then perhaps bash script                       #
+# TODO: Try rewriting logging function                                          #
+# TODO: Use different params for getrecenttracks (maybe)                        #
+# TODO: Use try catch pls
 #                                                                               #
 #################################################################################
 
@@ -28,13 +31,31 @@ USER_TO_LISTEN = configObj["userToListen"]
 
 GET_RECENT_TRACKS_URL = "%s?method=user.getrecenttracks&user=%s&api_key=%s&format=json" % (LAST_FM_API_BASE, USER_TO_LISTEN, LAST_FM_API_KEY)
 
+def recLog(s, tabs):
+
+
+
 def log(s):
     logger.debug(s)
 
 
 def main():
     resp = requests.get(GET_RECENT_TRACKS_URL)
-    log("Status code for GET_RECENT_TRACKS %s" % (str(resp.status_code)))
-    pass
+    respCode = resp.status_code
+
+    # Just some error handling
+    if respCode != 200:
+        log("Bad response")
+        exit()
+    # Logging for debugging purposes
+    log("Status code for GET_RECENT_TRACKS %s" % (str(respCode)))
+    respContent = resp.json()
+    log(respContent)
+    latestTrack = respContent["recenttracks"]["track"][0] 
+    isPlaying = respContent["recenttracks"]["track"][0]["@attr"]["nowplaying"]
+    if isPlaying == "true":
+        print("%s is currently listening to %s by %s" % (USER_TO_LISTEN, latestTrack["name"], latestTrack["artist"]["#text"]))
+    else:
+        print("%s is not currently listening to anything" % USER_TO_LISTEN)
 
 main()
