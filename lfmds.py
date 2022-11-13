29c4580b93115c43796c99ad5b2d6f55563abf50
William Berg <William.Berg2@gmail.com>
2022-11-13 13:40:03 +0100

    hopefully not commiting api key

--- lfmds.py
+++ lfmds.py
@@ -1,16 +1,40 @@
-#
-# TODO: Get auth
-# TODO: Find correct api
-# 
+#################################################################################
+#                                                                               #
+# TODO: Get auth                                                                #
+# TODO: Use command line inputs, then perhaps bash script                       #
+#                                                                               #
+#################################################################################
 
 import json
+import requests
+import logging
 
-LAST_FM_API_KEY = "hej lol" #Get from config.json, but first get a key 
+# Create and configure logger
+logging.basicConfig(filename="lfmdsLog.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+ 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+configObj = json.load(open('./config.json', 'r'))
+
+# Fun api stuff
+LAST_FM_API_KEY = configObj["apiKey"]  #Get from config.json, but first get a key 
 LAST_FM_API_BASE = "http://ws.audioscrobbler.com/2.0/"
 
-GET_RECENT_TRACKS_URL = "%s?method=user.getrecenttracks&user=rj&api_key=%s&format=json" % (LAST_FM_API_BASE, LAST_FM_API_KEY)
+# The user which is currently listening to music/is scrobbling
+USER_TO_LISTEN = configObj["userToListen"]
+
+GET_RECENT_TRACKS_URL = "%s?method=user.getrecenttracks&user=%s&api_key=%s&format=json" % (LAST_FM_API_BASE, USER_TO_LISTEN, LAST_FM_API_KEY)
+
+def log(s):
+    logger.debug(s)
+
 
 def main():
+    resp = requests.get(GET_RECENT_TRACKS_URL)
+    log("Status code for GET_RECENT_TRACKS %s" % (str(resp.status_code)))
     pass
 
 main()
